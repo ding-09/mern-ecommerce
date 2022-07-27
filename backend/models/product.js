0e1961a4403810d91f910dd35825df6ac1fbb531
Tiffany <yang.ding@hotmail.com>
2022-07-27 15:27:24 -0700

    simplified schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,36 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-    lowercase: true,
-    enum: ['women', 'men', 'kids'],
-  },
-  image: {
-    mobile: {
-      type: String,
-    },
-    tablet: {
-      type: String,
-    },
-    desktop: {
-      type: String,
-    },
-  },
+  title: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
 });
 
 const Product = mongoose.model('Product', productSchema);
