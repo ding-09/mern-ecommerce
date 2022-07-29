c91fc0a8400a1856f997a2fb574b308fe3a66c91
Tiffany <yang.ding@hotmail.com>
2022-07-29 12:38:51 -0700

    added animation to mobile menu

--- frontend/src/layouts/header/Header.jsx
+++ frontend/src/layouts/header/Header.jsx
@@ -4,6 +4,7 @@ import { MdOutlineShoppingBag } from 'react-icons/md';
 import { CgClose } from 'react-icons/cg';
 import { ReactComponent as Logo } from '../../assets/logos/logo.svg';
 import { StyledHeader, Nav, Menu } from './style';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   // toggle menu state
@@ -14,11 +15,17 @@ const Header = () => {
     setMenu(!menu);
   };
 
+  // framer motion animatin for menu toggle
+  const variants = {
+    open: { opacity: [0, 1] },
+    closed: { opacity: [0, 1] },
+  };
+
   return (
     <StyledHeader>
       <Nav>
         {/* toggle open/close button based on menu state */}
-        {menu ? (
+        {/* {menu ? (
           <button className='icon close-icon' aria-label='Close menu' onClick={toggleMenu}>
             <CgClose />
           </button>
@@ -26,7 +33,18 @@ const Header = () => {
           <button className='icon menu-icon' aria-label='Open menu' onClick={toggleMenu}>
             <HiOutlineMenuAlt4 />
           </button>
-        )}
+        )} */}
+        <motion.button
+          className='icon menu-icon'
+          animate={menu ? 'open' : 'closed'}
+          variants={variants}
+          initial={false}
+          transition={{ duration: 0.3 }}
+          onClick={toggleMenu}
+          aria-label={menu ? 'Close menu' : 'Open menu'}
+        >
+          {menu ? <CgClose /> : <HiOutlineMenuAlt4 />}
+        </motion.button>
         <a href='/'>
           <Logo />
         </a>
