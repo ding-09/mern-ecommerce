261a02b01ca8dae0f34bf917e970039c635eb061
Tiffany <yang.ding@hotmail.com>
2022-07-29 12:30:05 -0700

    added header and nav

--- frontend/src/layouts/header/Header.jsx
+++ frontend/src/layouts/header/Header.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+import { MdOutlineShoppingBag } from 'react-icons/md';
+import { CgClose } from 'react-icons/cg';
+import { ReactComponent as Logo } from '../../assets/logos/logo.svg';
+import { StyledHeader, Nav, Menu } from './style';
 
 const Header = () => {
-  return (
-    <header>
-      This is header
-    </header>
-  )
-}
+  // toggle menu state
+  const [menu, setMenu] = useState(false);
 
-export default Header
+  // toggle menu function
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
+  return (
+    <StyledHeader>
+      <Nav>
+        {/* toggle open/close button based on menu state */}
+        {menu ? (
+          <button className='icon close-icon' aria-label='Close menu' onClick={toggleMenu}>
+            <CgClose />
+          </button>
+        ) : (
+          <button className='icon menu-icon' aria-label='Open menu' onClick={toggleMenu}>
+            <HiOutlineMenuAlt4 />
+          </button>
+        )}
+        <a href='/'>
+          <Logo />
+        </a>
+        <a href='/cart' className='icon bag-icon'>
+          <MdOutlineShoppingBag />
+        </a>
+        {/* Opened Menu */}
+        {menu && (
+          <Menu>
+            <ul>
+              <li>
+                <a href='/women'>Women</a>
+              </li>
+              <li>
+                <a href='/men'>Men</a>
+              </li>
+              <li>
+                <a href='/kids'>Kids</a>
+              </li>
+              <li>
+                <a href='/sale'>Sale</a>
+              </li>
+            </ul>
+          </Menu>
+        )}
+      </Nav>
+    </StyledHeader>
+  );
+};
+
+export default Header;
---
+++ frontend/src/layouts/header/style.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  padding: 1.6rem 2.4rem;
+  display: flex;
+  border-bottom: 1px solid black;
+  position: relative;
+`;
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1;
+  button {
+    border: none;
+    background: none;
+  }
+  button,
+  a {
+    display: flex;
+  }
+  .icon {
+    font-size: 2.2rem;
+  }
+`;
+
+export const Menu = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  z-index: 99;
+  padding: 1.4rem 2.4rem;
+  background-color: var(--primary-white-bg);
+  li {
+    margin-bottom: 1.4rem;
+    padding-bottom: 0.6rem;
+    border-bottom: 1px solid var(--accent-dark);
+    a {
+      color: var(--secondary-dark-text);
+    }
+  }
+`;
