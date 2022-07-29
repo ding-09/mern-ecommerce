e4f23bf6219dde5871e8d6f0c8fac594c86aee07
Tiffany <yang.ding@hotmail.com>
2022-07-29 11:20:45 -0700

    added blank layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
-function App() {
-  fetch('http://localhost:8000/products')
-    .then((response) => response.json())
-    .then(data => console.log(data));
-  return <h1>E-Commerce</h1>;
-}
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// layouts
+import Header from './layouts/header'
+import Main from './layouts/main';
+import Footer from './layouts/footer';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />}>
+          {/* <Route index element={<HomePage />} />
+          <Route path='products'>
+            <Route path=':category' element={<ProductsPage />} />
+            <Route path='id/:productId' element={<ProductDetailsPage />} />
+          </Route> */}
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
