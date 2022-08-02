192b9d704fed720b2b8139873c85e519c12561f8
Tiffany <yang.ding@hotmail.com>
2022-08-02 12:42:33 -0700

    set up home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,17 @@ import Header from './layouts/header'
 import Main from './layouts/main';
 import Footer from './layouts/footer';
 
+// pages
+import Home from './pages/home'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path='/' element={<Main />}>
-          {/* <Route index element={<HomePage />} />
-          <Route path='products'>
+          <Route index element={<Home />} />
+          {/* <Route path='products'>
             <Route path=':category' element={<ProductsPage />} />
             <Route path='id/:productId' element={<ProductDetailsPage />} />
           </Route> */}
--- frontend/src/layouts/main/Main.jsx
+++ frontend/src/layouts/main/Main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-const Main = () => {
+const Main = ({ children }) => {
   return (
     <main>
-      Main page
+      {children}
+      <Outlet />
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
