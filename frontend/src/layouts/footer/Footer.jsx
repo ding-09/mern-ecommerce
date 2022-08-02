ec6b62fe4c09a7af3d911afb28914365cbb97b3c
Tiffany <yang.ding@hotmail.com>
2022-08-02 12:15:14 -0700

    added footer

--- frontend/src/layouts/footer/Footer.jsx
+++ frontend/src/layouts/footer/Footer.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { GridContainer, Socials } from './style';
+import { ReactComponent as Logo } from '../../assets/logos/logo.svg';
+import { ReactComponent as TwitterIcon } from '../../assets/icons/twitter.svg';
+import { ReactComponent as FacebookIcon } from '../../assets/icons/facebook.svg';
+import { ReactComponent as InstagramIcon } from '../../assets/icons/instagram.svg';
+import { ReactComponent as PinterestIcon } from '../../assets/icons/pinterest.svg';
+import { ReactComponent as YoutubeIcon } from '../../assets/icons/youtube.svg';
 
 const Footer = () => {
   return (
     <footer>
-      This is the footer
+      <GridContainer>
+        <a href='/'>
+          <Logo />
+        </a>
+        <section className='nav-links'>
+          <ul>
+            <li>
+              <a href='/women'>Women</a>
+            </li>
+            <li>
+              <a href='/men'>Men</a>
+            </li>
+            <li>
+              <a href='/kids'>Kids</a>
+            </li>
+            <li>
+              <a href='/sale'>Sale</a>
+            </li>
+          </ul>
+        </section>
+        <Socials>
+          <ul>
+            <li>
+              <a href='#'>
+                <TwitterIcon />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <FacebookIcon />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <InstagramIcon />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <PinterestIcon />
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <YoutubeIcon />
+              </a>
+            </li>
+          </ul>
+        </Socials>
+        <p className='copyright'>Copyright 2022. All rights reserved</p>
+      </GridContainer>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ frontend/src/layouts/footer/style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const GridContainer = styled.div`
+  padding: 4rem 0;
+  display: grid;
+  grid-gap: 2.4rem;
+  gap: 2.4rem;
+  text-align: center;
+  place-items: center;
+  background-color: var(--primary-dark-text);
+  a, .copyright {
+    color: var(--secondary-white-text);
+  }
+  .copyright {
+    opacity: 0.6;
+    font-size: 1rem;
+  }
+  svg {
+    path {
+      fill: var(--secondary-white-text);
+    }
+  }
+  .nav-links {
+    ul li {
+      padding-bottom: 0.6rem;
+    }
+  }
+`;
+
+export const Socials = styled.section`
+  ul {
+    display: flex; 
+    li {
+      margin: 0 0.6rem;
+    }
+  }
+`
